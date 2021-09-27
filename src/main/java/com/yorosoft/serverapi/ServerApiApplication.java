81dbc78a87fae8ecc4b55d27634d5b32f6e84c49
Codeur47 <yoropapers@gmail.com>
2021-09-27 13:42:59 +0000

    Setup CI/CD 19

--- src/main/java/com/yorosoft/serverapi/ServerApiApplication.java
+++ src/main/java/com/yorosoft/serverapi/ServerApiApplication.java
@@ -17,17 +17,19 @@ public class ServerApiApplication {
 		SpringApplication.run(ServerApiApplication.class, args);
 	}
 
+	private static final String CONTENT_ORIGIN = "Access-Control-Allow-Origin";
+
 	@Bean
 	public CorsFilter corsFilter() {
 		UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
 		corsConfiguration.setAllowedOrigins(Collections.singletonList("https://yorosoft-server-frontend.herokuapp.com/"));
-		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
+		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", CONTENT_ORIGIN, "Content-Type",
 				"Accept", "Jwt-Token", "Authorization", "Origin, Accept", "X-Requested-With",
 				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
 		corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Jwt-Token", "Authorization",
-				"Access-Control-Allow-Origin", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials", "Filename"));
+				CONTENT_ORIGIN, CONTENT_ORIGIN, "Access-Control-Allow-Credentials", "Filename"));
 		corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
 		urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
 		return new CorsFilter(urlBasedCorsConfigurationSource);
