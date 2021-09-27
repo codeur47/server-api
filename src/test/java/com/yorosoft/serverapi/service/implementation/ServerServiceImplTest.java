3a437213867fc904ad5c72391fcbaf8abf7497c9
Codeur47 <yoropapers@gmail.com>
2021-09-27 12:35:24 +0000

    Setup CI/CD 17

--- src/test/java/com/yorosoft/serverapi/service/implementation/ServerServiceImplTest.java
+++ src/test/java/com/yorosoft/serverapi/service/implementation/ServerServiceImplTest.java
@@ -18,18 +18,12 @@ import java.util.ArrayList;
 import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.domain.PageImpl;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.support.AnnotationConfigContextLoader;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = {ServerApiApplication.class, InMemoryDatabaseConfig.class})
