bb7a08fea0c2686ef0ee9d20cc81227e4b5ae4b0
Codeur47 <yoropapers@gmail.com>
2021-09-27 10:15:58 +0000

    Change 1

--- src/main/java/com/yorosoft/serverapi/model/Server.java
+++ src/main/java/com/yorosoft/serverapi/model/Server.java
@@ -5,21 +5,19 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
 
-import static javax.persistence.GenerationType.AUTO;
+import static javax.persistence.GenerationType.IDENTITY;
 
 @Data
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "server_crud")
 public class Server {
     @Id
-    @GeneratedValue(strategy = AUTO)
+    @GeneratedValue(strategy = IDENTITY)
     private Long id;
     @Column(unique = true)
     @NotEmpty(message = "IP Address cannot be empty or null")
