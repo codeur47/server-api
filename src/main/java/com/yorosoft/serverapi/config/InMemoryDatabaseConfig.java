61422ebaed125528f1ebae857c20ccb147af5bd0
Codeur47 <yoropapers@gmail.com>
2021-09-27 11:54:40 +0000

    Setup CI/CD 11

--- src/main/java/com/yorosoft/serverapi/config/InMemoryDatabaseConfig.java
+++ src/main/java/com/yorosoft/serverapi/config/InMemoryDatabaseConfig.java
@@ -1,5 +1,6 @@
 package com.yorosoft.serverapi.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,20 +11,21 @@ import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
+import java.util.Objects;
 
 @Configuration
-@EnableJpaRepositories(basePackages = "com.yorosoft.serverapi.repository")
-@PropertySource("application-dev.properties")
+@RequiredArgsConstructor
 @EnableTransactionManagement
+@PropertySource("application-dev.properties")
+@EnableJpaRepositories(basePackages = "com.yorosoft.serverapi.repository")
 public class InMemoryDatabaseConfig {
 
-    @Autowired
-    private Environment env;
+    private final Environment env;
 
     @Bean
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName(env.getProperty("spring.datasource.driver-class-name"));
+        dataSource.setDriverClassName(Objects.requireNonNull(env.getProperty("spring.datasource.driver-class-name")));
         dataSource.setUrl(env.getProperty("spring.datasource.url"));
         dataSource.setUsername(env.getProperty("spring.datasource.username"));
         dataSource.setPassword(env.getProperty("spring.datasource.password"));
